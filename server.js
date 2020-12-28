79298d7735e5e20a59a5fb15708b2cbd030758ba
Hantonny Korrea <hantonny.20@gmail.com>
2020-12-27 20:00:22 -0400

    :sparkles: - Configurando o MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' })
 
-require('dotenv').config({path:'variables.env'})
+//Conexão ao Banco de Dados || Database Connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error) => {
+    console.error("ERROR: "+error.message);
+})
 
 app.set('port', process.env.PORT || 7777);
 
